af43df6da04e9a538fac31e39c2eb564a370fbda
jjo23 <jjo23@kent.ac.uk>
2024-10-04 15:35:06 +0100

    Update partTwo.py

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,14 @@
 import math  
 
 def main():
-#TO DO  
+    num1 = int(input("enter a number: "))
+    num2 = int(input("enter another number: "))
+    pythag(num1, num2)
 
 def pythag(A,B):
-#TO DO  
+    sqr = A**2 + B**2
+    root = math.sqrt(sqr)
+    print(f"the length of the hyp is {root}")
 
 main()
+
